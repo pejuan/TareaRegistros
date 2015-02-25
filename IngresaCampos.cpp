481fa7b9c600cc3bf67ed0915ad0146815ce769f
Jose Pejuan <jose.pejuan@unitec.edu>
2015-02-25 10:08:10 -0600

    Funcion quicksort

--- IngresaCampos.cpp
+++ IngresaCampos.cpp
@@ -30,7 +30,7 @@ struct IndNum
 	int rrn;
 	int key;
 };
-
+static void quicksort(vector<IndNum>, int, int);
 int main(int argc, char** argv){
 	while(true){
 		vector<int> tipocampos;
@@ -1460,6 +1460,13 @@ int main(int argc, char** argv){
 				contadorRRN++;
 			}
 			inRRN.close();
+
+
+			///////////////////////////////////////////////////////////////////////
+
+
+			///////////////////////////////////////////////////////////////////////
+
 			
 		}
 
@@ -1469,4 +1476,27 @@ int main(int argc, char** argv){
 	
 
 	return 0;
+}
+
+static void quicksort(vector<IndNum> A, int izq, int der) {
+ int pivote=A[izq].key; // tomamos primer elemento como pivote
+ int i=izq; // i realiza la búsqueda de izquierda a derecha
+ int j=der; // j realiza la búsqueda de derecha a izquierda
+ int aux;
+
+ while(i<j){            // mientras no se crucen las búsquedas
+    while(A[i].key <=pivote && i<j) i++; // busca elemento mayor que pivote
+    while(A[j].key >pivote) j--;         // busca elemento menor que pivote
+    if (i<j) {                      // si no se han cruzado                      
+        aux= A[i].key;                  // los intercambia
+        A[i].key = A[j].key;
+        A[j].key = aux;
+    }
+  }
+  A[izq].key =A[j].key; // se coloca el pivote en su lugar de forma que tendremos
+  A[j].key =pivote; // los menores a su izquierda y los mayores a su derecha
+  if(izq<j-1)
+     quicksort(A,izq,j-1); // ordenamos subarray izquierdo
+  if(j+1 <der)
+     quicksort(A,j+1,der); // ordenamos subarray derecho
 }
