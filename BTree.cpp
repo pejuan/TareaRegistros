3347ea716a0a8b74aab99753c4a6bab889224cb0
Jose Pejuan <jose.pejuan@unitec.edu>
2015-03-24 19:48:55 -0600

    Nodo busca posición de la llave buscada

--- BTree.cpp
+++ BTree.cpp
@@ -5,10 +5,20 @@ using namespace std;
 struct NodeInts{
 	int data[15];
 	NodeInts* children[16];
+	int numero_llaves;
+
+	int buscarLlave(int llave){
+		int retval = 0;
+		while(retval<numero_llaves && llave > data[a]){
+			retval++;
+		}
+		return retval;
+	}
 };
 struct NodeStrings{
 	string data[15];
 	NodeStrings* children[16];
+	int numero_llaves;
 };
 
 BTree::BTree(){
@@ -27,5 +37,5 @@ int BTree::search_int(){
 
 }
 string BTree::search_string(){
-	
+
 }
